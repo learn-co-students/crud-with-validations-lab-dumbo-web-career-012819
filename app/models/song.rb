e5cfd61f01636167b708310ab5cd4042a87f06b6
Angelo Spampinato <spampinatodesign@gmail.com>
2019-02-25 17:30:24 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,13 @@
 class Song < ApplicationRecord
 	validates :title, presence: true, uniqueness: { scope: :release_year }
 	validates :released, inclusion: { in: [true, false] }
+	validates :release_year, presence: true, if: :released?
 	validates_each :release_year do |song, attr, value|
-		song.errors.add(attr, 'must be current year or earlier') if value == nil || value >= DateTime.now.year
+		song.errors.add(attr, 'must be current year or earlier') if value != nil && value >= DateTime.now.year 
 	end
 	validates :artist_name, presence: true
+
+	def released?
+		released
+	end
 end
